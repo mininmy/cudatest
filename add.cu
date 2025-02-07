a1b827c3d3af7d1678e9ff69770870c514bf8c95
mininmy <mykhailo.minin@gmail.com>
2025-02-07 17:03:17 +0200

    Update add.cu

--- add.cu
+++ add.cu
@@ -12,7 +12,7 @@ int main(void)
 {
   int N = 1<<20;
   float *x, *y;
-
+  std::cout << "Max error: ";
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&x, N*sizeof(float));
   cudaMallocManaged(&y, N*sizeof(float));
