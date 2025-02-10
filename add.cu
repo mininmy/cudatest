e32b1d4a9f841ce86886e358c1868b4185a51056
mininmy <mykhailo.minin@gmail.com>
2025-02-10 20:21:54 +0200

    Update add.cu

--- add.cu
+++ add.cu
@@ -1,42 +1,92 @@
 #include <stdio.h>
 
+//
+// Nearly minimal CUDA example.
+// Compile with:
+//
+// nvcc -o example example.cu
+//
+
+#define N 1000
+
+//
+// A function marked __global__
+// runs on the GPU but can be called from
+// the CPU.
+//
+// This function multiplies the elements of an array
+// of ints by 2.
+//
+// The entire computation can be thought of as running
+// with one thread per array element with blockIdx.x
+// identifying the thread.
+//
+// The comparison i<N is because often it isn't convenient
+// to have an exact 1-1 correspondence between threads
+// and array elements. Not strictly necessary here.
+//
+// Note how we're mixing GPU and CPU code in the same source
+// file. An alternative way to use CUDA is to keep
+// C/C++ code separate from CUDA code and dynamically
+// compile and load the CUDA code at runtime, a little
+// like how you compile and load OpenGL shaders from
+// C/C++ code.
+//
 __global__
-void saxpy(int n, float a, float *x, float *y)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i>n) y[i] = a*x[i] + y[i];
+void add(int *a, int *b) {
+    int i = blockIdx.x;
+    if (i<N) {
+        b[i] = 2*a[i];
+    }
 }
 
-int main(void)
-{
-  int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+int main() {
+    //
+    // Create int arrays on the CPU.
+    // ('h' stands for "host".)
+    //
+    int ha[N], hb[N];
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+    //
+    // Create corresponding int arrays on the GPU.
+    // ('d' stands for "device".)
+    //
+    int *da, *db;
+    cudaMalloc((void **)&da, N*sizeof(int));
+    cudaMalloc((void **)&db, N*sizeof(int));
 
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+    //
+    // Initialise the input data on the CPU.
+    //
+    for (int i = 0; i<N; ++i) {
+        ha[i] = i;
+    }
 
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+    //
+    // Copy input data to array on GPU.
+    //
+    cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice);
 
-  // Perform SAXPY on 1M elements
-  saxpy<<<1, 1>>>(N, 5.0f, d_x, d_y);
+    //
+    // Launch GPU code with N threads, one per
+    // array element.
+    //
+    add<<<N, 1>>>(da, db);
 
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+    //
+    // Copy output array from GPU back to CPU.
+    //
+    cudaMemcpy(hb, db, N*sizeof(int), cudaMemcpyDeviceToHost);
 
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = max(maxError, abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
+    for (int i = 0; i<N; ++i) {
+        printf("%d\n", hb[i]);
+    }
 
-  cudaFree(d_x);
-  cudaFree(d_y);
-  free(x);
-  free(y);
+    //
+    // Free up the arrays on the GPU.
+    //
+    cudaFree(da);
+    cudaFree(db);
+
+    return 0;
 }
